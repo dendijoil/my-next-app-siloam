aa40279dbfbcfddcb0523c482f23534d225bf9c8
dendijoil <dendijoil@gmail.com>
2024-08-25 18:18:55 +0700

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   basePath: '/my-next-app-siloam',
   assetPrefix: '/my-next-app-siloam/',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
